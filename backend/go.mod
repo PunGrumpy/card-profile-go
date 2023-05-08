8a471d8079348722cfd457330d6cb062e157c4f6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-08 21:56:32 +0700

    feat(*): api user's detail

---
+++ backend/go.mod
@@ -0,0 +1,9 @@
+module github.com/PunGrumpy/card-profile-go
+
+go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rs/cors v1.9.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+)
+
+type User struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("id")
+	user := User{
+		ID:          userID,
+		Name:        "Noppakorn Kaewsalabnil",
+		Description: "Full Stack Developer specializing in Golang, React, Typescript, and SCSS.",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	allowdOrigins, ok := os.LookupEnv("ALLOWED_ORIGINS")
+	if !ok {
+		log.Fatal("ALLOWED_ORIGINS is not set in .env file")
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/user", getUser).Methods("GET")
+
+	corsOptions := cors.New(cors.Options{
+		AllowedOrigins: []string{allowdOrigins},
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
+	handler := corsOptions.Handler(r)
+	log.Fatal(http.ListenAndServe(":8000", handler))
+}
